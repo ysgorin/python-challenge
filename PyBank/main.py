4c3ba7d4004a03341cecfcc2487ec7c18624efdd
ysgorin <ysgorin@gmail.com>
2023-01-08 22:44:32 -0500

    Cleaned up PyBank/main.py code | Updated README

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,103 +1,90 @@
-# In this Challenge, you are tasked with creating a Python script
-# to analyze the financial records of your company.
-# You will be given a financial dataset called budget_data.csv.
-# The dataset is composed of two columns: "Date" and "Profit/Losses".
-
-# Your task is to create a Python script that analyzes the records
-# to calculate each of the following values:
+# PyBank Challenge
 
 import os
 import csv
 
-total_months = 0
-months = []
-net_profit_losses = 0
-previous_amount = 0 
-changes = []
+# Total Number of Months
+ttl_mos = 0
 
+# List of Months
+mos = []
+
+# Net Total of Profits/Losses
+pnl_ttl = 0
+
+# Previous Month's Profit/Loss
+prev_amt = 0 
+
+# List of Monthly Change in Profits/Losses
+pnl_changes = []
+
+# File path of financial dataset
 resource_path = os.path.join("Resources", "budget_data.csv")
 
+# File path to export analysis
+output_path = os.path.join("analysis", "analysis.txt")
+
+# Analyze financial dataset
 with open(resource_path, 'r', encoding='utf') as file:
-    csvreader = csv.reader(file, delimiter=",")
-    
-    csv_header = next(csvreader)
-    print(csv_header)
+   csvreader = csv.reader(file, delimiter=",")
+   csv_header = next(csvreader)
 
-    # Get a sense of the data.
-    # print(next(csvreader))
-    # print(next(csvreader))
-    # print(next(csvreader))
+   for row in csvreader:
+        # Calculate total months
+        ttl_mos = ttl_mos +1
+        
+        # Create list of months
+        mos.append(row[0])
+        
+        # Calculate net total of profits/losses
+        pnl_ttl = pnl_ttl + int(row[1])
+        
+        # Calculate monthly change in profits/losses
+        change = int(row[1]) - prev_amt
+        
+        # Create list of monthly changes
+        pnl_changes.append(change)
+        
+        # Set previous amount variable for next row
+        prev_amt = int(row[1])
 
-    for row in csvreader:
-        total_months = total_months +1
-        months.append(row[0])
-        net_profit_losses = net_profit_losses + int(row[1])
-        change = int(row[1]) - previous_amount
-        changes.append(change)
-        previous_amount = int(row[1])
+# Analyze monthly changes in profits/losses
+# Remove first values from pnl_changes and mos.
+pnl_changes.pop(0)
+mos.pop(0)
 
-# remove first change from changes because it is the first value.
-# remove first month for same reason
-changes.pop(0)
-months.pop(0)
-average_change = round((sum(changes)/len(changes)),2)
-greatest_increase = max(changes)
-greatest_decrease = min(changes)
+# Calculate average change
+avg_change = round((sum(pnl_changes)/len(pnl_changes)),2)
 
-changes_and_months = zip(months, changes)
+# Calculate greatest increase and decrease
+gr_inc = max(pnl_changes)
+gr_dec = min(pnl_changes)
 
-for row in changes_and_months:
-    if row[1] == greatest_increase:
-        g_increase_month = row[0]
-    elif row[1] == greatest_decrease:
-        g_decrease_month = row[0]
+# Identify month of greatest increase and decrease
+pnl_changes_and_mos = zip(mos, pnl_changes)
+for row in pnl_changes_and_mos:
+   if row[1] == gr_inc:
+       gr_inc_mo = row[0]
+   elif row[1] == gr_dec:
+       gr_dec_mo = row[0]
 
-# print(total_months)
-# print(net_profit_losses)
-# print(average_change)
-# print(f"{g_increase_month} - {greatest_increase}")
-# print(f"{g_decrease_month} - {greatest_decrease}")
-
+# Print analysis results to terminal
 print(f"""
-
 Financial Analysis
 ----------------------------
-Total Months: {total_months}
-Total: ${net_profit_losses}
-Average Change: ${average_change}
-Greatest Increase in Profits: {g_increase_month} (${greatest_increase})
-Greatest Decrease in Profits: {g_decrease_month} (${greatest_decrease})
-
+Total Months: {ttl_mos}
+Total: ${pnl_ttl}
+Average Change: ${avg_change}
+Greatest Increase in Profits: {gr_inc_mo} (${gr_inc})
+Greatest Decrease in Profits: {gr_dec_mo} (${gr_dec})
 """)
 
-output_path = os.path.join("analysis", "analysis.txt")
+# Print analysis results to .txt file
 with open(output_path, 'w') as a:
-    print(f"""Financial Analysis
+   print(f"""Financial Analysis
 ----------------------------
-Total Months: {total_months}
-Total: ${net_profit_losses}
-Average Change: ${average_change}
-Greatest Increase in Profits: {g_increase_month} (${greatest_increase})
-Greatest Decrease in Profits: {g_decrease_month} (${greatest_decrease})""", file=a)
-
-# The total number of months included in the dataset
-
-# The net total amount of "Profit/Losses" over the entire period
-
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-# The greatest increase in profits (date and amount) over the entire period
-
-# The greatest decrease in profits (date and amount) over the entire period
-
-# Your analysis should align with the following results:
-
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $22564198
-# Average Change: $-8311.11
-# Greatest Increase in Profits: Aug-16 ($1862002)
-# Greatest Decrease in Profits: Feb-14 ($-1825558)
-
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+Total Months: {ttl_mos}
+Total: ${pnl_ttl}
+Average Change: ${avg_change}
+Greatest Increase in Profits: {gr_inc_mo} (${gr_inc})
+Greatest Decrease in Profits: {gr_dec_mo} (${gr_dec})""", file=a)
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # python-challenge
+
+This repo contains two scripts, PyBank and PyPoll.
+
+PyBank
+
+This author was tasked with creating a Python script that analyzes a financial dataset.
+The dataset contains two columns: "Date" and "Profit/Losses."
+The following are calculated with the PyBank script:
+* Total number of months in the dataset
+* Net total amount of "Profit/Losses" over the entire period
+* Average monthly change in "Profit/Losses"
+* Greatest increase in profits over the entire period
+* Greatest descrease in profits over the entire period
+
+Results are printed in the terminal and outputted to a .txt file.
+
+PyPoll
