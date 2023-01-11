8910ff0bc9a0d5a3818733553ff7d6b4969b7857
ysgorin <ysgorin@gmail.com>
2023-01-10 21:04:31 -0500

    code cleaned and commented

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,36 +3,47 @@
 import os
 import csv
 
+# Total Votes
 ttl_votes = 0
+# Dictionary - Candidate Name, Votes Won
 dict_candidates = {}
 
 # File path of election data
 resource_path = os.path.join("Resources", "election_data.csv")
-
 # File path to export analysis
 output_path = os.path.join("analysis", "analysis.txt")
 
+# Analyze election dataset
 with open(resource_path, 'r', encoding='utf') as data:
     csvreader = csv.reader(data, delimiter=',')
+    
+    # Store the header row
     csv_header = next(csvreader)
 
     for row in csvreader:
+        # Calculate total votes
         ttl_votes = ttl_votes + 1
         if row[2] not in dict_candidates:
+            # Create dictionary key for identified candidate and start votes won tally
             dict_candidates[row[2]] = 0
+        # Add one to the candidate's votes tally
         dict_candidates[row[2]] = dict_candidates[row[2]] +1
 
+# Print election results to terminal
 print(f"""
 Election Results
 -------------------------
 Total Votes: {ttl_votes}
 -------------------------""")
 for k, v in dict_candidates.items():
+    # Candidates Name: Percent of Votes Won (Number of Votes Won)
     print(f"{k}: {round(((v/ttl_votes)*100),3)}% ({v})")
+# Candidate with the most votes won
 print(f"""-------------------------
 Winner: {max(dict_candidates, key=dict_candidates.get)}
 -------------------------""")
 
+# Print election results to .txt file
 with open(output_path, 'a') as a:
     print(f"""Election Results
 -------------------------
